708088b08d0d597e37210ad714cbdf0e32d19094
Student <nat.g.dimitrova@gmail.com>
2024-01-10 13:03:23 +0200

    forgottenPassword.html updated, but not fully functioning

--- validations.js
+++ validations.js
@@ -123,19 +123,16 @@ function validateChangePassword() {
         new_password_change.classList.remove('is-invalid');
         help_change.classList.add('d-none');
     }
-
-    if (!forgotten_pass) {
-        if (new_password_change_value == old_password_change_value) {
-            new_password_change.classList.add('is-invalid');
-            help_change.classList.remove('d-none');
-            instruction_change.innerHTML = String("Старата парола съответства на новата парола!");
-            return;
-        }
-        else {
-            new_password_change.classList.remove('is-invalid');
-            help_change.classList.add('d-none');
-            instruction_change.innerHTML = null;
-        }
+    if (new_password_change_value == old_password_change_value) {
+        new_password_change.classList.add('is-invalid');
+        help_change.classList.remove('d-none');
+        instruction_change.innerHTML = String("Новата парола съответства на старата парола!");
+        return;
+    }
+    else {
+        new_password_change.classList.remove('is-invalid');
+        help_change.classList.add('d-none');
+        instruction_change.innerHTML = null;
     }
 
     //Confirm Password 
@@ -143,6 +140,7 @@ function validateChangePassword() {
         confirm_password_change.classList.add('is-invalid');
         help_change.classList.remove('d-none');
         instruction_change.innerHTML = String("Въведените пароли не съответстват!");
+        return;
     }
     else {
         confirm_password_change.classList.remove('is-invalid');
@@ -155,13 +153,93 @@ function validateChangePassword() {
     document.getElementById('form-change-password').reset();
 }
 
-function forgottenPassword(forgotten) {
-    let old_password_change_container = document.getElementById('old-password-change-container');
-    if (forgotten) {
-        old_password_change_container.classList.add('d-none');
+var step = 1;
+function forgottenPassword() {
+    //Трябва да имам отделна променлива за имейл, за да знам на кой имейл сменям паролата
+
+    if (step == 1) { //Попълване на имейл и изпращане на код
+        let email_forgotten_value = document.getElementById('email-forgotten').value;
+        let email_forgotten = document.getElementById('email-forgotten');
+        if (!true) {  //сравнява с e-mails от api            
+            email_forgotten.classList.add('is-invalid');
+            document.getElementById('help-email-forgotten').classList.remove('d-none');
+            document.getElementById('instruction-email-forgotten').innerHTML = String("Акаунт с такъв e-mail НЕ съществува!");
+            return;
+        }
+        else {
+            email_forgotten.classList.remove('is-invalid');
+            document.getElementById('help-email-forgotten').classList.add('d-none');
+            document.getElementById('instruction-email-forgotten').innerHTML = null;
+
+            document.getElementById('form-email-forgotten').classList.add('d-none');
+            document.getElementById('form-code-forgotten').classList.remove('d-none');
+            step++;
+        }
     }
-    else {
-        old_password_change_container.classList.remove('d-none');
+    else if (step == 2) { //Сравнение на код
+        let code_forgotten_value = document.getElementById('code-forgotten').value;
+        let code_forgotten = document.getElementById('code-forgotten');
+        if (!true) { //сравнява код
+            code_forgotten.classList.add('is-invalid');
+            document.getElementById('help-code-forgotten').classList.remove('d-none');
+            document.getElementById('instruction-code-forgotten').innerHTML = String("Кодът не съответства на пратения код!");
+            return;
+        }
+        else {
+            code_forgotten.classList.remove('is-invalid');
+            document.getElementById('help-code-forgotten').classList.add('d-none');
+            document.getElementById('instruction-code-forgotten').innerHTML = null;
+
+            document.getElementById('form-code-forgotten').classList.add('d-none');
+            document.getElementById('form-pass-forgotten').classList.remove('d-none');
+            step++;
+            /*how to add data-bs-dismiss="modal"*/
+        }
     }
-    forgotten_pass = forgotten;
+
+    else if (step == 3) { //Въвеждане на новата парола и потвърждаване
+        let new_password_forgotten_value = document.getElementById('new-password-forgotten').value;
+        let new_password_forgotten = document.getElementById('new-password-forgotten');
+
+        let confirm_password_forgotten_value = document.getElementById('confirm-password-forgotten').value;
+        let confirm_password_forgotten = document.getElementById('confirm-password-forgotten');
+
+        let help_pass_forgotten = document.getElementById('help-pass-forgotten');
+        let instruction_pass_forgotten = document.getElementById('instruction-pass-forgotten');
+
+        if (new_password_forgotten_value.length < 8) {
+            new_password_forgotten.classList.add('is-invalid');
+            help_pass_forgotten.classList.remove('d-none');
+            instruction_pass_forgotten.innerHTML = String("Новата паролата трябва да е поне 8 символа.");
+            return;
+        }
+        else {
+            //Продължава
+            new_password_forgotten.classList.remove('is-invalid');
+            help_pass_forgotten.classList.add('d-none');
+            instruction_pass_forgotten.innerHTML = null;
+        }
+
+        //Confirm Password 
+        if (new_password_forgotten_value !== confirm_password_forgotten_value) {
+            confirm_password_forgotten.classList.add('is-invalid');
+            help_pass_forgotten.classList.remove('d-none');
+            instruction_pass_forgotten.innerHTML = String("Въведените пароли не съответстват!");
+            return;
+        }
+        else {
+            confirm_password_forgotten.classList.remove('is-invalid');
+            help_pass_forgotten.classList.add('d-none');
+            instruction_pass_forgotten.innerHTML = null;
+
+            //Изписаните данни ще се изпращат към API
+            console.log(new_password_change_value);
+        }
+        alert("Успешно смеихте паролата си!");
+        document.getElementById('form-pass-forgotten').classList.add('d-none');
+        document.getElementById('form-email-forgotten').classList.remove('d-none');
+        step = 1;
+
+    }
+
 }
